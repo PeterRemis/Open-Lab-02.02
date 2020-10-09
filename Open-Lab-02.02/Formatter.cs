4006417e769b6c339788478598d650939985e741
PeterRemis <remis@spsknm.sk>
2020-10-09 23:33:29 +0200

    ADD SIMPLE SOLUTION

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace Open_Lab_02._02
 {
@@ -6,7 +7,38 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+
+            // riesenie A
+            /*
+            for(int i=0;i<numbers.Length-1;i++)
+            {
+                Console.Write(Math.Round(numbers[i], 2).ToString(System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
+                Console.Write(" ");
+            }
+            Console.Write(Math.Round(numbers[numbers.Length-1], 2).ToString(System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
+            */
+
+            // riesenie B
+            /*
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                Console.Write(numbers[i].ToString("#.##", System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
+                Console.Write(" ");
+            }
+            Console.Write(numbers[numbers.Length-1].ToString("#.##", System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
+            */
+
+            // riesenie C
+            string[] newArray = new string[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newArray[i] = numbers[i].ToString("#.##");
+            }
+            string message = string.Join(' ', newArray);
+            message = message.Replace(',' , '.');
+            Console.Write(message);
+
+
         }
     }
 }
