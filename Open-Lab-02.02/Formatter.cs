1d5be78c264317fe3a27ef9ef11af8a87052dfd9
PeterRemis <remis@spsknm.sk>
2020-10-10 08:38:27 +0200

    Pridaj stvrty variant (D)

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Threading.Tasks.Dataflow;
 
 namespace Open_Lab_02._02
 {
@@ -7,26 +8,26 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-
-            // riesenie A
             /*
+            // riesenie A
+            
             for(int i=0;i<numbers.Length-1;i++)
             {
                 Console.Write(Math.Round(numbers[i], 2).ToString(System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
                 Console.Write(" ");
             }
             Console.Write(Math.Round(numbers[numbers.Length-1], 2).ToString(System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
-            */
+            
 
             // riesenie B
-            /*
+            
             for (int i = 0; i < numbers.Length - 1; i++)
             {
                 Console.Write(numbers[i].ToString("#.##", System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
                 Console.Write(" ");
             }
             Console.Write(numbers[numbers.Length-1].ToString("#.##", System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
-            */
+       
 
             // riesenie C
             string[] newArray = new string[numbers.Length];
@@ -37,8 +38,18 @@ namespace Open_Lab_02._02
             string message = string.Join(' ', newArray);
             message = message.Replace(',' , '.');
             Console.Write(message);
+            */
 
-
+            // riesenie D
+            string message = "";
+            foreach (double i in numbers)
+            {
+                message = message + string.Format("{0:0.00} ", i);
+            }
+            message = message.Replace(',', '.');
+            message = message.Trim();
+            Console.Write(message);
+            
         }
     }
 }
